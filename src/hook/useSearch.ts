3658d9b61d73fa2dd6f9d2a4b2bc810dd722d45d
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-04-24 21:37:12 -0600

    casa not found hook search

--- src/hook/useSearch.ts
+++ src/hook/useSearch.ts
@@ -1,23 +1,22 @@
 import {useState} from 'react';
 import { Movies } from '@/store/movieStore';
 import { generalMovie, searchMovies } from '@/api/getMovie';
-
-
-
-const [search, setSearch] = useState("");
-const setMovies = Movies((state) => state.setMovies);
+import { MovieResult } from '@/models/MovieResult'
 
 export const useSearch = () =>{
 
+const [search, setSearch] = useState('');
+const setMovies = Movies((state) => state.setMovies);
+
       const data = async () => {
-        let getAllMovies = [];
+        let getAllMovies: MovieResult = [];
         
         if (search) {
             getAllMovies = await searchMovies(search);
 
         } else{
-
-            allMovies.status === 200 ? setMovies(allMovies.data.results) : console.log('error');
+            const {data, status} = await generalMovie()
+            status === 200 ? setMovies(data.results) : console.log('error');
         }
       };
 
