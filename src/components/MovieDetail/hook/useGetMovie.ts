e66311da1e9d4220080a06b2f3bae099487b5b89
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-04-27 21:23:14 -0600

    amost editing css

---
+++ src/components/MovieDetail/hook/useGetMovie.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+import { generalMovie } from '@/api/getMovie';
+import { Results } from '@/models/Results';
+
+export const useGetMovie = (id:string) =>{
+
+const [movie, setMovie] = React.useState<Results>({
+  adult: false,
+  backdrop_path: '',
+  genre_ids: [],
+  id: 0,
+  media_type: '',
+  original_language: '',
+  original_title: '',
+  overview: '',
+  popularity: 0,
+  poster_path: '',
+  release_date: '',
+  title: '',
+  video: false,
+  vote_average: 0,
+  vote_count: 0
+});
+
+React.useEffect(() => {
+  const data = async () => {
+    const { status, data } = await generalMovie(id);
+    status === 200 ? setMovie(data) : console.log('error');
+  };
+  data();
+}, [id]);
+
+return { movie };
+}
--- src/components/MovieDetail/index.tsx
+++ src/components/MovieDetail/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { generalMovie } from '@/api/getMovie';
-import { Results } from '@/models/Results'
 import { URLIMG } from '@/models/UrlMovie';
+import { useGetMovie } from './hook/useGetMovie';
 
 interface id {
   id:string
@@ -9,15 +8,7 @@ interface id {
 
 const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
   
-  const [movie, setMovie] = React.useState<Results>({});
-
-     React.useEffect(() => {
-         const data = async () => {
-           const { status, data } = await generalMovie(id);
-           status === 200 ? setMovie(data) : console.log('error');
-         };
-         data();
-     }, [id]);
+  const { movie } = useGetMovie(id);
 
   return (
     <div className="w-full h-[100vh]">
@@ -26,11 +17,11 @@ const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
         alt=""
         className="-z-0 w-full h-[400px] object-cover shadow-xl rounded-b-3xl"
       />
-      <p className="absolute top-0 right-0 w-16 h-10 bg-black text-white">{movie.vote_average}</p>
+      <p className="border absolute w-36 h-7 top-5 -right-11 bg-white text-lg font-extrabold text-center rotate-45">
+        {movie.vote_average.toFixed(1)}
+      </p>
       <div className="relative z-20 flex flex-col h-[400px] items-center mt-5">
-        <h3 className="font-semibold text-2xl text-center">
-          {movie.title}
-        </h3>
+        <h3 className="font-semibold text-2xl text-center">{movie.title}</h3>
         <p>Release Date: {movie.release_date}</p>
         <h3 className="font-semibold text-xl mt-3">Description</h3>
         <p className="text-justify text-gray-500 m-4">{movie.overview}</p>
