fc4f109f139051af197e794f49e3bb683fa82d1e
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-05-03 15:52:53 -0600

    addin icons date

--- src/components/MovieDetail/index.tsx
+++ src/components/MovieDetail/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { URLIMG } from '@/models/UrlMovie';
 import { useGetMovie } from './hook/useGetMovie';
 import GoBack from '../GoBack';
+import {CalendarIcon} from '@heroicons/react/20/solid'
 
 interface id {
   id:string
@@ -24,8 +25,10 @@ const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
       />
       <div className="absolute z-20 -mt-10 rounded-t-3xl bg-white p-4">
         <h3 className="font-semibold text-2xl text-center mt-5">{movie.title}</h3>
-        
-        <p className=''>{movie.release_date}</p>
+        <div className='flex items-center'>
+          <CalendarIcon className=" text-lime-500 h-6 w-6 m-4"/>
+          <p className="">{movie.release_date}</p>
+        </div>
         <p className="text-justify text-gray-500">{movie.overview}</p>
       </div>
     </div>
