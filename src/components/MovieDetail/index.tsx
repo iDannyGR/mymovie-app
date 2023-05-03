3db0ea20aeec9fe16f11a77bedb869b27bef4262
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-05-02 20:47:21 -0600

    minor fixed

--- src/components/MovieDetail/index.tsx
+++ src/components/MovieDetail/index.tsx
@@ -13,6 +13,7 @@ const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
 
   return (
     <div className="relative w-full h-[100vh] overflow-x-hidden">
+      <GoBack />
       <p className="z-20 absolute w-36 h-7 top-6 -right-8 bg-white text-lg font-extrabold text-center rotate-45 shadow-inner shadow-black">
         {movie.vote_average.toFixed(1)}
       </p>
@@ -21,13 +22,12 @@ const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
         alt=""
         className="-z-10 w-full h-[450px] object-cover shadow-xl"
       />
-      <div className="absolute z-20 -mt-10 flex flex-col items-center rounded-t-3xl bg-white">
-        <h3 className="font-semibold text-2xl text-center">{movie.title}</h3>
-        <p>Release Date: {movie.release_date}</p>
-        <h3 className="font-semibold text-xl mt-3">Description</h3>
-        <p className="text-justify text-gray-500 m-4">{movie.overview}</p>
+      <div className="absolute z-20 -mt-10 rounded-t-3xl bg-white p-4">
+        <h3 className="font-semibold text-2xl text-center mt-5">{movie.title}</h3>
+        
+        <p className=''>{movie.release_date}</p>
+        <p className="text-justify text-gray-500">{movie.overview}</p>
       </div>
-      <GoBack />
     </div>
   );
 }
