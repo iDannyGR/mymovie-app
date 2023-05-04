c58d44cc09996b69c1a2708e906a8d3073b5119c
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-05-03 21:47:24 -0600

    adding data from mmovie detail and extend interface movie

--- src/components/MovieDetail/index.tsx
+++ src/components/MovieDetail/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { URLIMG } from '@/models/UrlMovie';
 import { useGetMovie } from './hook/useGetMovie';
 import GoBack from '../GoBack';
-import {CalendarIcon} from '@heroicons/react/20/solid'
+import { CalendarIcon } from '@heroicons/react/20/solid'; 
+import { ClockIcon } from '@heroicons/react/20/solid'; 
 
 interface id {
   id:string
@@ -11,7 +12,6 @@ interface id {
 const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
   
   const { movie } = useGetMovie(id);
-
   return (
     <div className="relative w-full h-[100vh] overflow-x-hidden">
       <GoBack />
@@ -24,12 +24,18 @@ const MovieDetail:React.FC<id> = ({id}):React.ReactElement => {
         className="-z-10 w-full h-[450px] object-cover shadow-xl"
       />
       <div className="absolute z-20 -mt-10 rounded-t-3xl bg-white p-4">
-        <h3 className="font-semibold text-2xl text-center mt-5">{movie.title}</h3>
-        <div className='flex items-center'>
-          <CalendarIcon className=" text-lime-500 h-6 w-6 m-4"/>
+        <h3 className="font-semibold text-2xl text-center">{movie.title}</h3>
+        <div className="flex items-center mt-3">
+          <CalendarIcon className=" text-lime-500 h-6 w-6 mr-3" />
           <p className="">{movie.release_date}</p>
         </div>
-        <p className="text-justify text-gray-500">{movie.overview}</p>
+        <div className="flex items-center mt-3">
+          <ClockIcon className=" h-6 w-6 text-lime-500 mr-3" />
+          <p>
+            {Math.floor(movie.runtime / 60)} : {movie.runtime % 60} hours
+          </p>
+        </div>
+        <p className="text-justify text-gray-500 mt-6">{movie.overview}</p>
       </div>
     </div>
   );
